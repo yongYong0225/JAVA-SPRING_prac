07b967ce12dded3cd26a76e5713bbeca01744fab
yongYong0225 <118151866+yongYong0225@users.noreply.github.com>
2022-12-16 21:00:34 +0900

    .

---
+++ part_1/Fc_Chap01/src/ch05/VariableTest.java
@@ -0,0 +1,16 @@
+package ch05;
+
+public class VariableTest {
+
+    public static void main(String[] args) {
+
+        byte bnum = 127;
+
+        System.out.println(bnum);
+
+        long lNum = 12345678900L;
+
+        System.out.println(lNum);
+
+    }
+}
---
+++ part_2/Fc_Chap02/src/Ch03/FuntionTest.java
@@ -0,0 +1,7 @@
+package Ch03;
+
+public class FuntionTest {
+    
+
+
+}
